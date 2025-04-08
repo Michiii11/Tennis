38d09b79c056c1bc91ed1d2938dbe7a8cb0c6487
Michael Leisch <michael.leisch@gmx.at>
2025-04-08 14:41:02 +0200

    normal itn calculator finsihed

--- itnRechner/script.js
+++ itnRechner/script.js
@@ -11,25 +11,93 @@ function selectSingleOrDoubleMode(elem){
 
     amountOfFields = 2
     activeType = "S"
+
+    let rows = ""
+
     if(elem.innerHTML === "Doppel") {
         amountOfFields = 4;
         activeType = "D";
     }
 
+    if(activeType === "S"){
+        rows = `
+            <div class="row">
+                <p>${names[0]} (0.000)</p>
+                <p></p>
+            </div>
+            <div class="row">
+                <p>${names[1]} (0.000)</p>
+                <p></p>
+            </div>
+        `
+    } else {
+        rows = `
+            <div class="row">
+                <p>${names[0]} (0.000)</p>
+                <p></p>
+            </div>
+            <div class="row">
+                <p>${names[1]} (0.000)</p>
+                <p></p>
+            </div>
+            <div class="row">
+                <p>${names[2]} (0.000)</p>
+                <p></p>
+            </div>
+            <div class="row">
+                <p>${names[3]} (0.000)</p>
+                <p></p>
+            </div>
+        `
+    }
+
     let itnFields = ""
-    let rows = ""
+
     for (let i = 0; i < amountOfFields; i++) {
         itnFields += `<div class="itnField">
                         <input type="text" placeholder="10.000" value="${itns[i] || ''}">
-                        <select name="sik" id="sikSelect1">
-                            <option value="Sicher">Sicher</option>
-                            <option value="Nicht Sicher">Nicht Sicher</option>
-                        </select>
+                        <div class="select" id="sikSelect${i+1}">
+                            <div class="selected">
+                                <p>Sicher</p>
+                                <i class="fa-solid fa-chevron-down"></i>
+                            </div>
+                            <div class="options">
+                                <p class="option">Sicher</p>
+                                <p class="option">Nicht Sicher</p>
+                            </div>
+                        </div>
                  </div>`
     }
     document.querySelector(".itnFields").innerHTML = itnFields
     document.querySelector(".changedITNField.winner .rows").innerHTML = rows
     document.querySelector(".changedITNField.loser .rows").innerHTML = rows
+
+    const customSelect = document.querySelectorAll('.select');
+
+    customSelect.forEach((select) => {
+        const selected = select.querySelector('.selected');
+        const selectedP = select.querySelector('.selected p')
+        const options = select.querySelector('.options');
+
+        selected.addEventListener('click', () => {
+            options.style.display = options.style.display === 'block' ? 'none' : 'block';
+        });
+
+        options.addEventListener('click', (e) => {
+            if (e.target.classList.contains('option')) {
+                selectedP.textContent = e.target.textContent;
+                selected.dataset.value = e.target.dataset.value;
+                options.style.display = 'none';
+                handleChanges()
+            }
+        });
+
+        document.addEventListener('click', (e) => {
+            if (!select.contains(e.target)) {
+                options.style.display = 'none';
+            }
+        });
+    })
 
     document.querySelectorAll("input, select").forEach((elem) => {
         elem.addEventListener("keyup", function (event) {
@@ -57,8 +125,8 @@ function handleChanges(){
         }
     })
 
-    document.querySelectorAll(`select`).forEach((elem) => {
-        siks.push(elem.selectedOptions[0].label);
+    document.querySelectorAll(`.select`).forEach((elem) => {
+        siks.push(elem.querySelector(".selected p").innerHTML);
     })
 
     if (areAllInputsFilled) {
@@ -106,6 +174,9 @@ function handleChanges(){
  * @returns {string[]} itn of each player after the game
  */
 function calcItnSingle(p1, p2, sik1, sik2, hasRetired){
+    p1 = p1.replaceAll(",", ".")
+    p2 = p2.replaceAll(",", ".")
+
     let calculatedITNs = []
     sik1 = sik1 === "Sicher" ? 1 : 0;
     sik2 = sik2 === "Sicher" ? 1 : 0;
@@ -152,6 +223,11 @@ function calcItnSingle(p1, p2, sik1, sik2, hasRetired){
  * @returns {string[]} itn of each player after the game
  */
 function calcItnDoubles(p1, p2, p3, p4, hasRetired){
+    p1 = p1.replaceAll(",", ".")
+    p2 = p2.replaceAll(",", ".")
+    p3 = p3.replaceAll(",", ".")
+    p4 = p4.replaceAll(",", ".")
+
     let calculatedITNs = []
 
     if(hasRetired){ // Check if game has finished because of retirement
@@ -215,3 +291,22 @@ function continueWithWinOrLose(elem){
 
     selectSingleOrDoubleMode(document.querySelector(".toggle p.active"))
 }
+
+
+
+function handleScreenChange(e) {
+    if (e.matches) {
+        // Screen width is less than 1200px
+        document.querySelector('nav.mobile').classList.add('active')
+        document.querySelector('nav.desktop').classList.remove('active')
+    } else {
+        // Screen width is 1200px or greater
+        document.querySelector('nav.mobile').classList.remove('active')
+        document.querySelector('nav.desktop').classList.add('active')
+    }
+}
+
+const mediaQuery = window.matchMedia("(max-width: 1199px)");
+mediaQuery.addEventListener("change", handleScreenChange);
+
+handleScreenChange(mediaQuery);
